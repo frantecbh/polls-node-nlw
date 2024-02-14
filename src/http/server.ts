a2fedd56970c80fc9350986b2c06cf14cb99269e
frantecbh <franciscomenezesbh@gmail.com>
2024-02-14 00:03:39 -0300

    rota post

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,28 @@
 import fasttify from 'fastify'
+import z from 'zod'
+import { prisma } from '../database/prisma'
+
 
 const app = fasttify()
 
 
-app.get('/hello', () =>{
-  return 'ola frantec!'
+app.post('/polls', async (request, replay) =>{
+
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+ const {title} = createPollBody.parse(request.body)
+  
+
+ const poll = await prisma.poll.create({
+  data:{
+    title
+  }
+ })
+
+
+  return replay.status(201).send({pollId: poll.id}) 
 })
 
 
