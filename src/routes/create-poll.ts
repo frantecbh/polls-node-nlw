38d58b942f1aaf04bd6014c1ed74d86e4272a2c2
frantecbh <franciscomenezesbh@gmail.com>
2024-02-14 01:49:28 -0300

    create table options polls

--- src/routes/create-poll.ts
+++ src/routes/create-poll.ts
@@ -1,27 +1,31 @@
-import z from "zod"
-import { prisma } from "../database/prisma"
-import { FastifyInstance } from "fastify"
-
-
+import z from 'zod'
+import { prisma } from '../database/prisma'
+import { FastifyInstance } from 'fastify'
 
 export async function createPoll(app: FastifyInstance) {
-  app.post('/polls', async (request, replay) =>{
-
+  app.post('/polls', async (request, replay) => {
     const createPollBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options: z.array(z.string()),
     })
-  
-   const {title} = createPollBody.parse(request.body)
-    
-  
-   const poll = await prisma.poll.create({
-    data:{
-      title
-    }
-   })
-  
-  
-    return replay.status(201).send({pollId: poll.id}) 
+
+    const { title, options } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+      data: {
+        title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return {
+                title: option,
+              }
+            }),
+          },
+        },
+      },
+    })
+
+    return replay.status(201).send({ pollId: poll.id })
   })
-  
 }
